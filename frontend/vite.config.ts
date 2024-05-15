0882d0150f87fab192f24169c0be997ce4129d70
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-05-15 20:10:17 +0200

    Added comment so its known where is globals import

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import sassGlobImport from "vite-plugin-sass-glob-import";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), sassGlobImport()],
   css: {
     preprocessorOptions: {
       scss: {
+        // Globals are imported in main.tsx
         additionalData: `@import "./src/styles/_variables.scss"; @import "./src/styles/_mixins.scss";`,
       },
     },
