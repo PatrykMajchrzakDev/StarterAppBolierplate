29d35a04928fe43da818d4312c1134bb599284c3
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-05-17 18:42:50 +0200

    Added auth routes and controllers

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const SECRET =
+  process.env.JWT_SECRET || "rMk,E(6SvKw;5q=[CTf!pN+?hY<d@$.Ha47B%8zg";
+
+// Helper function to type guard
+const isErrorWithMessage = (error: any): error is { message: string } => {
+  return typeof error === "object" && error !== null && "message" in error;
+};
+
+// Register user when called from frontend
+export const register = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser)
+      return res.status(400).json({ error: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        role: "USER", // Assign 'USER' role by default
+      },
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    if (isErrorWithMessage(error)) {
+      res.status(400).json({ error: error.message });
+    } else {
+      res.status(400).json({ error: "An unexpected error occurred" });
+    }
+  }
+};
+
+// Login user when called from frontend
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword)
+      return res.status(401).json({ error: "Invalid Password" });
+
+    const token = jwt.sign({ userId: user.id, role: user.role }, SECRET);
+    res.json({ token, role: user.role });
+  } catch (error) {
+    if (isErrorWithMessage(error)) {
+      res.status(400).json({ error: error.message });
+    } else {
+      res.status(400).json({ error: "An unexpected error occurred" });
+    }
+  }
+};
