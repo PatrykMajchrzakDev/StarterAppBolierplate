f59ce6513b85f8bf34f545f27e2cb36ed0169676
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-05-17 18:41:26 +0200

    Migration prisma

---
+++ backend/prisma/migrations/20240517150934_inititial_setup/migration.sql
@@ -0,0 +1,15 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
