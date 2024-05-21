d5bf483a43454a5b8e4ed8ada54500bb94031dee
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-05-21 20:51:42 +0200

    Added route to redirect to login / register

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -7,6 +7,8 @@ import { Route, Routes, Navigate } from "react-router-dom";
 // ======= COMPONENTS =========
 // ============================
 import Layout from "./layouts/Layout";
+import SignIn from "@/components/Login/SignIn";
+import Register from "@/components/Login/SignUp";
 
 const AppRoutes = () => {
   return (
@@ -17,7 +19,29 @@ const AppRoutes = () => {
       {/* ====================== */}
       {/* ======= ROUTES ======= */}
       {/* ====================== */}
+
+      {/* ======= ROOT =======*/}
       <Route path="/" element={<Layout>HOME PAGE</Layout>}></Route>
+
+      {/*======= AUTH =======*/}
+      <Route
+        path="/signin"
+        element={
+          <Layout>
+            <SignIn />
+          </Layout>
+        }
+      ></Route>
+      <Route
+        path="/register"
+        element={
+          <Layout>
+            <Register />
+          </Layout>
+        }
+      ></Route>
+
+      {/* ======= USER ======= */}
       <Route
         path="/user-profile"
         element={<span>USER PROFILE PAGE</span>}
