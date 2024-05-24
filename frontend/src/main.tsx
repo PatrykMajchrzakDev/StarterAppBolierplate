cf100cc99ccaca2932d25248e1dda3559aa63c55
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-05-24 20:52:47 +0200

    Added tanstack provider for handling queries

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,16 +4,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
 import "./styles/_globals.scss";
+
 // ============================
 // ======= COMPONENTS =========
 // ============================
 import AppRoutes from "./AppRoutes";
 
+// Create a QueryClient instance
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-      <AppRoutes />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>
 );
