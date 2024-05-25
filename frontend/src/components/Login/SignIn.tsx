23bce1f49058c637195551252946b5e3b6843e35
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-05-25 12:24:32 +0200

    Almost finished SignIn form. Just have to make 'stay logged in work'

--- frontend/src/components/Login/SignIn.tsx
+++ frontend/src/components/Login/SignIn.tsx
@@ -1,50 +1,62 @@
-// ============================
 // ========= MODULES ==========
-// ============================
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { signInInputSchema } from "@/lib/auth";
+import { useLogin } from "@/lib/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
 // ============================
 // ======= COMPONENTS =========
 // ============================
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+} from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-
-function Copyright(props: any) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-      sx={{ fontSize: "1.4rem", mt: 2 }}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
 const SignIn = () => {
+  type FormData = z.infer<typeof signInInputSchema>;
+  const [apiError, setApiError] = useState<string | null>(null);
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormData>({
+    resolver: zodResolver(signInInputSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const { mutateAsync: loginUser, isLoading } = useLogin();
   // SEND DATA
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+  const onSubmit = async (data: FormData) => {
+    try {
+      console.log(data);
+      await loginUser(data);
+      navigate("/");
+      // TBD Handle successful registration (e.g., redirect, display success message)
+    } catch (error) {
+      if (error instanceof Error) {
+        setApiError(error.message);
+      } else {
+        setApiError("An unknown error occurred.");
+      }
+    }
   };
 
   return (
@@ -64,71 +76,86 @@ const SignIn = () => {
         <Typography component="h1" variant="h5" sx={{ fontSize: "1.7rem" }}>
           Sign in
         </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            InputLabelProps={{ style: { fontSize: "1.7rem" } }}
-            inputProps={{ style: { fontSize: "1.7rem" } }}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            InputLabelProps={{ style: { fontSize: "1.7rem" } }}
-            inputProps={{ style: { fontSize: "1.7rem" } }}
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                value="remember"
-                color="primary"
-                sx={{
-                  "& .MuiSvgIcon-root": {
-                    fontSize: 20,
-                  },
-                }}
-              />
-            }
-            label="Remember me"
-            sx={{
-              "& .MuiFormControlLabel-label": {
-                fontSize: "1.7rem",
-              },
-            }}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2, fontSize: "1.7rem" }}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2" sx={{ fontSize: "1.3rem" }}>
-                Forgot password?
-              </Link>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box sx={{ maxWidth: 400, mx: "auto", p: 3 }}>
+            {apiError && (
+              <Typography color="error" gutterBottom>
+                {apiError}
+              </Typography>
+            )}
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Email Address"
+              {...register("email")}
+              error={!!errors.email}
+              helperText={errors.email?.message}
+              autoComplete="email"
+              autoFocus
+              InputLabelProps={{ style: { fontSize: "1.7rem" } }}
+              inputProps={{ style: { fontSize: "1.7rem" } }}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              type="password"
+              label="Password"
+              {...register("password")}
+              error={!!errors.password}
+              helperText={errors.password?.message}
+              autoComplete="current-password"
+              InputLabelProps={{ style: { fontSize: "1.7rem" } }}
+              inputProps={{ style: { fontSize: "1.7rem" } }}
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  value="remember"
+                  color="primary"
+                  sx={{
+                    "& .MuiSvgIcon-root": {
+                      fontSize: 20,
+                    },
+                  }}
+                />
+              }
+              label="Remember me"
+              sx={{
+                "& .MuiFormControlLabel-label": {
+                  fontSize: "1.7rem",
+                },
+              }}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              disabled={isSubmitting || isLoading}
+              sx={{ mt: 3, mb: 2, fontSize: "1.7rem" }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2" sx={{ fontSize: "1.3rem" }}>
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link
+                  href="/signup"
+                  variant="body2"
+                  sx={{ fontSize: "1.3rem" }}
+                >
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link href="/signup" variant="body2" sx={{ fontSize: "1.3rem" }}>
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
+          </Box>
+        </form>
       </Box>
       <Copyright sx={{ mt: 8, mb: 4 }} />
     </Container>
@@ -136,3 +163,22 @@ const SignIn = () => {
 };
 
 export default SignIn;
+
+function Copyright(props: any) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+      sx={{ fontSize: "1.4rem", mt: 2 }}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
