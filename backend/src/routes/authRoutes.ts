705c0f9642af267a22cd517eac6535a73a499d9b
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-05-27 19:10:26 +0200

    Updated routes with /me to verify user

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
-import { register, login, test2 } from "../controllers/authController";
+import {
+  register,
+  login,
+  test2,
+  getUserDetails,
+} from "../controllers/authController";
+import { auth } from "@/middleware/Auth/Authorization";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/me", auth, getUserDetails);
 router.post("/test2", test2);
 
 export default router;
