deb7a9723f5a05e39906237cba7ced2d28644daa
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-05-30 12:37:04 +0200

    Removed queryClient setup because it was invoked twice. Second one in src/app/main-provider.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,18 +4,11 @@ import ReactDOM from "react-dom/client";
 
 import "./styles/_globals.scss";
 
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-
 // ======= COMPONENTS =========
 import App from "./app";
 
-// Create a QueryClient instance
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
