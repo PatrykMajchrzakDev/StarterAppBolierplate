b7e604b0e81b915c7e8cea1c8f66d6cce7007388
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-05-30 10:39:35 +0200

    Fixed MUI links to be react-router-dom links. It previously refreshed site as they acted as plain anochors

--- frontend/src/store/Theme/ThemeStore.ts
+++ frontend/src/store/Theme/ThemeStore.ts
@@ -9,15 +9,14 @@ interface ThemeState {
 }
 
 const ThemeStore = create<ThemeState>((set, get) => ({
-  theme: (localStorage.getItem("theme") as "light" | "dark") || "light", // Initialize state from localStorage or default to 'light'
+  theme: (localStorage.getItem("theme") as "light" | "dark") || "light",
   muiTheme: localStorage.getItem("theme") === "dark" ? darkTheme : lightTheme,
   toggleTheme: () => {
     const newTheme = get().theme === "light" ? "dark" : "light";
     const newMuiTheme = newTheme === "dark" ? darkTheme : lightTheme;
     set({ theme: newTheme, muiTheme: newMuiTheme });
-    // Optionally update localStorage
+
     localStorage.setItem("theme", newTheme);
-    // Optionally update DOM attribute if using for CSS variable-based theming
     document.documentElement.setAttribute("data-theme", newTheme);
   },
 }));
