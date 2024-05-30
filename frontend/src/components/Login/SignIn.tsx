1a301c34356df3f5a8c5bb2b0e4e6baa22f2b3f1
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-05-30 12:35:45 +0200

    Updated login and register forms to disable button when doing request and commented components

--- frontend/src/components/Login/SignIn.tsx
+++ frontend/src/components/Login/SignIn.tsx
@@ -28,12 +28,20 @@ import {
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 const SignIn = () => {
+  // FormData type set to indicate what input this form should hold
   type FormData = z.infer<typeof signInInputSchema>;
+
   const navigate = useNavigate();
+
+  // useForm component methods from react-hook-form package
   const {
+    // register on an input field, it tells react-hook-form to track and manage the value of that input.
+    // If you are using a resolver like zodResolver with a schema (as in your example), register will
+    // ensure that validation rules are applied to the input fields according to the schema.
     register,
     handleSubmit,
-    formState: { errors, isSubmitting },
+    // Current form state to handle different state like isLoading
+    formState: { errors },
   } = useForm<FormData>({
     resolver: zodResolver(signInInputSchema),
     defaultValues: {
@@ -42,10 +50,11 @@ const SignIn = () => {
     },
   });
 
-  const { mutateAsync: loginUser, isLoading } = useLogin();
+  const { mutateAsync: loginUser, status } = useLogin();
   // SEND DATA
   const onSubmit = async (data: FormData) => {
     try {
+      // If OK then show notification and navigate to /app
       await loginUser(data);
       navigate("/app");
       useNotificationState
@@ -67,6 +76,7 @@ const SignIn = () => {
           alignItems: "center",
         }}
       >
+        {/* Upper side of form - icon and text */}
         <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
           <LockOutlinedIcon />
         </Avatar>
@@ -74,8 +84,10 @@ const SignIn = () => {
           Sign in
         </Typography>
 
+        {/* Form with inputs */}
         <form onSubmit={handleSubmit(onSubmit)}>
           <Box sx={{ maxWidth: 400, mx: "auto", p: 3 }}>
+            {/* EMAIL FIELD */}
             <TextField
               margin="normal"
               required
@@ -87,6 +99,7 @@ const SignIn = () => {
               autoComplete="email"
               autoFocus
             />
+            {/* PASSWORD FIELD*/}
             <TextField
               margin="normal"
               required
@@ -98,6 +111,7 @@ const SignIn = () => {
               helperText={errors.password?.message}
               autoComplete="current-password"
             />
+            {/* REMEMBER ME FIELD */}
             <FormControlLabel
               control={
                 <Checkbox
@@ -115,15 +129,18 @@ const SignIn = () => {
                 "& .MuiFormControlLabel-label": {},
               }}
             />
+            {/* SUBMIT BUTTON */}
             <Button
               type="submit"
               fullWidth
               variant="contained"
-              disabled={isSubmitting || isLoading}
+              disabled={status === "pending"}
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign In
+              {status === "pending" ? "Signing in..." : "Sign in"}
             </Button>
+
+            {/* NAVIGATION LINKS */}
             <Grid container>
               <Grid item xs>
                 <Link to="#" variant="body2" component={RouterLink}>
--- frontend/src/components/Login/SignUp.tsx
+++ frontend/src/components/Login/SignUp.tsx
@@ -25,13 +25,18 @@ import {
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 const SignUp = () => {
-  // Define types for form data
+  // FormData type set to indicate what input this form should hold
   type FormData = z.infer<typeof signUpInputSchema>;
   const navigate = useNavigate();
   const {
+    // register on an input field, it tells react-hook-form to track and manage the value of that input.
+    // If you are using a resolver like zodResolver with a schema (as in your example), register will
+    // ensure that validation rules are applied to the input fields according to the schema.
     register,
     handleSubmit,
-    formState: { errors, isSubmitting },
+
+    // Current form state to handle different state like isLoading
+    formState: { errors },
   } = useForm<FormData>({
     resolver: zodResolver(signUpInputSchema),
     defaultValues: {
@@ -42,10 +47,11 @@ const SignUp = () => {
     },
   });
 
-  const { mutateAsync: registerUser, isLoading } = useRegister();
+  const { mutateAsync: registerUser, status } = useRegister();
 
   const onSubmit = async (data: FormData) => {
     try {
+      // If OK then show notification and navigate to /app
       await registerUser(data);
       navigate("/signin");
       useNotificationState
@@ -67,16 +73,19 @@ const SignUp = () => {
           alignItems: "center",
         }}
       >
+        {/* Upper side of form - icon and text */}
         <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5" sx={{ textAlign: "center" }}>
           Sign up
         </Typography>
+        {/* Form with inputs */}
         <form onSubmit={handleSubmit(onSubmit)}>
           <Box sx={{ maxWidth: 400, mx: "auto", p: 3 }}>
             <Grid container spacing={2}>
               <Grid item xs={12}>
+                {/* EMAIL FIELD */}
                 <TextField
                   required
                   label="Email"
@@ -88,6 +97,7 @@ const SignUp = () => {
                 />
               </Grid>
               <Grid item xs={12}>
+                {/* NAME FIELD */}
                 <TextField
                   required
                   label="Name"
@@ -99,6 +109,7 @@ const SignUp = () => {
                 />
               </Grid>
               <Grid item xs={12}>
+                {/* PASSWORD FIELD */}
                 <TextField
                   required
                   label="Password"
@@ -111,6 +122,7 @@ const SignUp = () => {
                 />
               </Grid>
               <Grid item xs={12}>
+                {/* CONFIRM PASSWORD FIELD */}
                 <TextField
                   required
                   label="Confirm Password"
@@ -123,17 +135,18 @@ const SignUp = () => {
                 />
               </Grid>
             </Grid>
-
+            {/* SUBMIT BUTTON */}
             <Button
               type="submit"
               fullWidth
               variant="contained"
-              disabled={isSubmitting || isLoading}
+              disabled={status === "pending"}
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign In
+              {status === "pending" ? "Signing up..." : "Sign up"}
             </Button>
           </Box>
+          {/* NAVIGATION LINKS */}
           <Grid container>
             <Grid item xs>
               <Link to="#" component={RouterLink} variant="body2">
