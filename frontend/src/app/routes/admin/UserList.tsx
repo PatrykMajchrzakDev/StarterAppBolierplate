2227ad2cf6496a58e42f76c9da2336b68645133a
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-06-06 17:54:22 +0200

    Updated component to correctly display spinner when fetching

--- frontend/src/app/routes/admin/UserList.tsx
+++ frontend/src/app/routes/admin/UserList.tsx
@@ -1,7 +1,7 @@
 import { useUsers } from "@/features/users/api/get-users";
 
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import CircularProgress from "@mui/material/CircularProgress";
+import { CircularProgress, Box } from "@mui/material";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 100 },
@@ -35,28 +35,37 @@ const UserList = ({ shouldFetch }: UserListProps) => {
   });
 
   if (isFetching) {
-    return <CircularProgress size="lg" />;
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height={400}
+        width="100%"
+      >
+        <CircularProgress size={50} />
+      </Box>
+    );
   }
 
   if (status === "error") {
     return <div>Failed to load data</div>;
   }
-
-  if (!data) return null;
-  console.log(data);
   return (
     <div style={{ height: 400, width: "100%" }}>
-      <DataGrid
-        rows={data}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-      />
+      {data && (
+        <DataGrid
+          rows={data}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+        />
+      )}
     </div>
   );
 };
