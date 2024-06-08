f6b0a3ff64f29960416a269c7609a4a3d58268c5
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-06-08 13:02:26 +0200

    Updated routes to access changing avatar fn

--- backend/src/routes/usersRoutes.ts
+++ backend/src/routes/usersRoutes.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
-import { getAllUsers } from "@/controllers/users/usersController";
+import {
+  getAllUsers,
+  changeUserAvatar,
+} from "@/controllers/users/usersController";
 import { auth, isAdmin } from "@/middleware/Auth/Authorization";
+const multer = require("multer");
 
+// Multer init
+const upload = multer({ storage: multer.memoryStorage() });
+
+// Router init
 const router = Router();
 
 // Get all users
 router.get("/getAllUsers", auth, isAdmin, getAllUsers);
+
+// Change user avatar
+router.post("/changeUserAvatar", auth, upload.single("file"), changeUserAvatar);
 
 export default router;
