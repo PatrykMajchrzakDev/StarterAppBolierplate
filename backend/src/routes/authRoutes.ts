30b5a12deaa8ad5e6cb5195a29e7b89a96e14d07
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-06-14 19:44:02 +0200

    Updated routes with email verification

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import {
-  register,
-  login,
-  test2,
-  getUserDetails,
-} from "@/controllers/auth/authController";
+import { test2, getUserDetails } from "@/controllers/auth/userDetails";
 import { auth } from "@/middleware/Auth/Authorization";
+import { register } from "@/controllers/auth/register";
+import { login } from "@/controllers/auth/login";
+import { resetPassword } from "@/controllers/auth/resetPassword";
+import {
+  resendVerificationEmail,
+  verifyEmail,
+} from "@/controllers/auth/verifyRegister";
 
 const router = Router();
 // Register user
@@ -16,6 +18,14 @@ router.post("/login", login);
 
 // Get user details
 router.get("/me", auth, getUserDetails);
+
+// Handle user verification token that is sent to user email
+router.get("/verify-email", verifyEmail);
+
+// Handle resend registration user token
+router.post("/resend-verification-email", resendVerificationEmail);
+
+router.post("/reset-password", resetPassword)
 
 // Test route to check if route works
 router.get("/test2", test2);
