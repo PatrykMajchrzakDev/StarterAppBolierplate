b0a5d9539ca63f86e90443a91ddc5ba071a10d2c
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-06-18 18:02:46 +0200

    Added / moved files to handle here only logged in user header

---
+++ frontend/src/components/Layout/Header/AppHeader/Header.tsx
@@ -0,0 +1,36 @@
+// This component functionality is to header - logo, theme toggler and navigation for main app
+
+// ============================
+// ========= MODULES ==========
+// ============================
+import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
+// ============================
+// ======= COMPONENTS =========
+// ============================
+import ThemeToggler from "@/components/UI/ThemeToggler/ThemeToggler";
+import MobileNav from "@/components/UI/MobileNavigation/MobileNav";
+import MainNav from "@/components/UI/TopNavigation/AppNavigation/MainNav";
+
+const Header = () => {
+  return (
+    <div id={styles.header}>
+      <div className={styles.wrapper}>
+        <Link to="/" className={styles.logo}>
+          MY APP LOGO
+        </Link>
+        <div className={styles.navActions}>
+          <ThemeToggler id="1" />
+          <div className={styles.mainNav}>
+            <MainNav />
+          </div>
+          <div className={styles.mobileNav}>
+            <MobileNav />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
