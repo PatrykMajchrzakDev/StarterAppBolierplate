35751ca34cdbc4cdb7f2ac06651c97d159f0d5ce
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-07-06 11:35:00 +0200

    Added condition to stop sending emails to users than are not created via local

--- backend/src/controllers/auth/verifyRegister.ts
+++ backend/src/controllers/auth/verifyRegister.ts
@@ -25,6 +25,13 @@ export const resendVerificationEmail = async (req: Request, res: Response) => {
       return res.status(400).json({ error: "User is already verified" });
     }
 
+    // only local users can put in password so check if password exists
+    if (user.provider !== "LOCAL") {
+      throw new Error(
+        "User email verification is only possible to previously created local users"
+      );
+    }
+
     // Generate new email verification token
     const verificationToken = crypto.randomBytes(32).toString("hex");
     const tokenExpiresAt = addHours(new Date(), 48); // Token expires in 48 hours
