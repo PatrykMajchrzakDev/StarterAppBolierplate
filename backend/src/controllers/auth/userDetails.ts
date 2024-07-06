7cfccf0f6ffe299686abbad9588015a6f1a369a6
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-07-06 11:34:01 +0200

    Removed redundant code and removed some user details passed when passing them via response to frontend

--- backend/src/controllers/auth/userDetails.ts
+++ backend/src/controllers/auth/userDetails.ts
@@ -1,13 +1,6 @@
 import { Request, Response } from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-import crypto from "crypto";
-
-// SECRET is used to later create token using this string
-const SECRET =
-  process.env.JWT_SECRET || "rMk,E(6SvKw;5q=[CTf!pN+?hY<d@$.Ha47B%8zg";
 
 // Helper function to type guard
 const isErrorWithMessage = (error: any): error is { message: string } => {
@@ -38,26 +31,13 @@ export const getUserDetails = async (req: Request, res: Response) => {
     }
 
     // Destructure user object to exclude password
-    const { password: _, ...userWithoutPassword } = user;
+    const { password, verificationToken, ...userWithoutSensitiveInfo } = user;
 
     // If all good then return response with user info and token
-    res.json({ user: userWithoutPassword, token });
+    res.json({ user: userWithoutSensitiveInfo, token });
   } catch (error) {
     res
       .status(500)
       .json({ error: "An error occurred while fetching user details" });
   }
-};
-
-// Just a test routing
-export const test2 = async (req: Request, res: Response) => {
-  console.log(crypto.randomBytes(5).toString("hex"));
-  // console.log("test2 accessed");
-  const user = {
-    id: "123asdasd",
-    name: "test2",
-    role: "USER",
-  };
-  const token = "1231231133121";
-  res.json({ user, token });
 };
