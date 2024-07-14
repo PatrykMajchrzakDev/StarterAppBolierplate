cfc4a6b462a7dd40051d6a7d1f9253a258b14adb
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2024-07-14 18:55:15 +0200

    Removed unnecessary margin

--- frontend/src/components/UI/User/UserProfileTooltip.tsx
+++ frontend/src/components/UI/User/UserProfileTooltip.tsx
@@ -51,7 +51,6 @@ const UserProfileTooltip = () => {
         <IconButton
           onClick={handleClick}
           size="small"
-          sx={{ ml: 2 }}
           aria-controls={open ? "account-menu" : undefined}
           aria-haspopup="true"
           aria-expanded={open ? "true" : undefined}
