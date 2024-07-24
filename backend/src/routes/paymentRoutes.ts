6269722669f2080de2efdee542b27c2d7a7a272e
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-07-24 20:49:56 +0200

    Updated route with 'subscribe' route

--- backend/src/routes/paymentRoutes.ts
+++ backend/src/routes/paymentRoutes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { makePayment } from "@/controllers/payment/makePayment";
+import { subscribe } from "@/controllers/payment/subscribe";
 
 const router = Router();
 
-// Stripe - make payment route
+// Stripe - make single payment route
 router.post("/make-payment", makePayment);
+
+router.get("/subscribe", subscribe);
 
 export default router;
