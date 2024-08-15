e528def10c9e531dd0427436f4ad575980c1b1b7
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-08-15 16:47:01 +0200

    Removed route of removed unnecessary makePayment file

--- backend/src/routes/paymentRoutes.ts
+++ backend/src/routes/paymentRoutes.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
-import { makePayment } from "@/controllers/payment/makePayment";
 import { subscribe } from "@/controllers/payment/subscribe";
 
 const router = Router();
 
-// Stripe - make single payment route
-router.post("/make-payment", makePayment);
-
+// Subscribe - payment
 router.get("/subscribe", subscribe);
 
 export default router;
