77803cccb8ca7a1c11d28ced3585703c4bf04983
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-08-15 16:46:39 +0200

    Added stripe checkout to process payment and update user role based on choosen role

--- backend/src/controllers/payment/subscribe.ts
+++ backend/src/controllers/payment/subscribe.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
 import Stripe from "stripe";
 import { getLogger } from "@/utils/logger";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
+
+import { Role } from "@prisma/client";
+const logger = getLogger("STRIPE_PAYMENT");
 // Stripe config
 const stripe = new Stripe(process.env.TEST_STRIPE_API_KEY || "", {
   apiVersion: "2024-06-20",
@@ -9,34 +14,66 @@ const stripe = new Stripe(process.env.TEST_STRIPE_API_KEY || "", {
 
 // Function when user clicks make payment in frontend
 export const subscribe = async (req: Request, res: Response, next: any) => {
-  // Log payment
-  const logger = getLogger("STRIPE_PAYMENT");
-  //   use provided options from frontend
+  //   Use provided options from frontend
+  const plan = req.query.plan as string;
+  const userId = req.query.userId as string;
 
-  const plan = req.query.plan;
+  // Check for plan and userId params
+  if (!userId || typeof userId !== "string") {
+    return res.redirect(`${process.env.FRONTEND_BASE_URL}/payment/cancelled`);
+  }
 
   if (!plan || typeof plan !== "string") {
     return res.redirect(`${process.env.FRONTEND_BASE_URL}/payment/cancelled`);
   }
 
   let priceId;
+  // Role is imported from Prisma schema
+  let newRole: Role;
 
   //   Those plans are taken from stripe PRODUCT CATALOG https://dashboard.stripe.com/products
   switch (plan.toLowerCase()) {
     case "subscriber":
       // THIS IS TEST MODE
       priceId = "price_1Pg9tx2MOnaYCpHvHE6amLjg";
+      newRole = Role.SUBSCRIBER;
       //   priceId = "price_1Pg8ka2MOnaYCpHv5oArQseW";
       break;
     case "pro":
       // THIS IS TEST MODE
       priceId = "price_1Pg9u72MOnaYCpHvrEDURmye";
+      newRole = Role.PRO;
       //   priceId = "price_1Pg8lE2MOnaYCpHvY64L57h7";
       break;
 
     default:
       return res.redirect(`${process.env.FRONTEND_BASE_URL}/payment/cancelled`);
   }
+
+  // Function to update user role when payment is marked as 'paid'
+  const pollPaymentStatus = async (sessionId: string, userId: string) => {
+    let paymentComplete = false;
+
+    while (!paymentComplete) {
+      // Retrieve the checkout session (await to ensure sequential execution)
+      const checkoutSession = await stripe.checkout.sessions.retrieve(
+        sessionId
+      );
+
+      if (checkoutSession.payment_status === "paid") {
+        // Update the user's role in the database
+        await prisma.user.update({
+          where: { id: userId },
+          data: { role: newRole },
+        });
+
+        paymentComplete = true;
+      } else {
+        // Wait for a few seconds before checking again
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      }
+    }
+  };
 
   try {
     const session = await stripe.checkout.sessions.create({
@@ -52,8 +89,21 @@ export const subscribe = async (req: Request, res: Response, next: any) => {
       // Cancelled redirect
       cancel_url: `${process.env.FRONTEND_BASE_URL}/payment/cancelled`,
     });
+
+    pollPaymentStatus(session.id, userId);
+
+    // Log payment
+    logger.info(
+      `New payment via Stripe || Client ID: ${session.client_reference_id}
+       UserID: ${userId} NewRole: ${newRole} Date: ${new Date()} Session ID: ${
+        session.id
+      }`
+    );
+
+    // Send the session URL to the client
     res.status(200).json(session.url);
   } catch (error) {
-    console.log(error);
+    console.error("Error during checkout process:", error);
+    res.status(500).send("An error occurred during the checkout process.");
   }
 };
