aaa9277f2157059e043ebf7d4349c1763e6f2cce
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-08-21 20:09:00 +0200

    Added axios api request function to cancel user's payment

---
+++ frontend/src/features/payments/cancelSubscription.ts
@@ -0,0 +1,43 @@
+// This file holds a function which makes a request to backend to cancel user's subscription
+
+import axios from "axios";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+export const cancelSubscription = async (
+  subscriptionId: string,
+  userId: string,
+  token: string
+) => {
+  try {
+    // Request to backend to cancel user's subscription payment
+    const response = await axios.post(
+      `${apiUrl}/payment/cancelSubscription`,
+      {
+        subscriptionId,
+        userId,
+        token,
+      },
+      // Header has to be provided as well to verify token on the backend
+      {
+        headers: {
+          Authorization: `Bearer ${token}`, // Include the token in the Authorization header
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      // Handle Axios-specific errors
+      console.error("Axios error:", error.response?.data || error.message);
+      throw new Error(
+        error.response?.data?.error || "Failed to cancel subscription"
+      );
+    } else {
+      // Handle non-Axios errors
+      console.error("Unexpected error:", error);
+      throw new Error(
+        "An unexpected error occurred while canceling the subscription"
+      );
+    }
+  }
+};
