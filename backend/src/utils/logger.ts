4cff68428d3dabacbb1b50a6ef1fe20cc9757a96
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-08-22 18:40:17 +0200

    Updated logger fn to also expect name of file it should save logs in

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,34 +1,43 @@
 import { createLogger, transports, format } from "winston";
 
-// Create the base logger with common configuration
-export const baseLogger = createLogger({
-  level: "info",
-
-  // Define the log format
-  format: format.combine(
-    format.timestamp(), // Add timestamp to logs
-    format.errors({ stack: true }), // Include error stack traces
-    format.splat(), // Support for string interpolation
-    format.json() // Format logs as JSON
-  ),
-  // Define where to save the log files
-  transports: [
-    new transports.File({ filename: "error.log", level: "error" }), // Log errors to error.log
-    new transports.File({ filename: "combined.log" }), // Log all levels to combined.log
-  ],
-});
+// Create a function to generate the base logger with a specific file name
+const createBaseLogger = (filename: string) => {
+  return createLogger({
+    level: "info",
+    format: format.combine(
+      format.timestamp(), // Add timestamp to logs
+      format.errors({ stack: true }), // Include error stack traces
+      format.splat(), // Support for string interpolation
+      format.json() // Format logs as JSON
+    ),
+    transports: [
+      new transports.File({ filename: "error.log", level: "error" }), // Log errors to error.log
+      new transports.File({ filename: `${filename}.log` }), // Log all levels to the specified file
+    ],
+  });
+};
 
 // Add console logging in non-production environments
-if (process.env.NODE_ENV !== "production") {
-  baseLogger.add(
-    new transports.Console({
-      format: format.simple(), // Use simple format for console logs
-    })
-  );
-}
+const addConsoleTransport = (logger: any) => {
+  if (process.env.NODE_ENV !== "production") {
+    logger.add(
+      new transports.Console({
+        format: format.simple(), // Use simple format for console logs
+      })
+    );
+  }
+};
 
-// Function to create a child logger with specific metadata
-export const getLogger = (serviceName: string) => {
+// Function to create a child logger with specific metadata and an optional filename
+export const getLogger = (
+  serviceName: string,
+  fileName: string = "combined"
+) => {
+  const logger = createBaseLogger(fileName);
+
+  // Add console transport if needed
+  addConsoleTransport(logger);
+
   // Return a child logger with the service name as metadata
-  return baseLogger.child({ defaultMeta: { service: serviceName } });
+  return logger.child({ defaultMeta: { service: serviceName } });
 };
